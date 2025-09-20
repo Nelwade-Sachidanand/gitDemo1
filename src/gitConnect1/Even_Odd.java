77068f077a3a8ee3e83a05207e0fb3d1e182d702
Nelwade-Sachidanand <nelwadesachin14@gmail.com>
2025-09-20 03:55:03 -0300

    Even odd program

---
+++ src/gitConnect1/Even_Odd.java
@@ -0,0 +1,15 @@
+package gitConnect1;
+
+import java.util.Scanner;
+
+public class Even_Odd {
+	public static void main(String[] args) {
+		Scanner se=new Scanner(System.in);
+		int num=se.nextInt();
+		if(num%2==0) {
+			System.out.println("even number");
+		}else {
+			System.out.println("odd number");
+		}
+	}
+}
